78822ab1440252e5f44c50de6231d0db7967c36d
igor <fibih.igor94@yandex.ru>
2023-07-18 14:56:57 +0300

    Task №4

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 8  Напише программу, которая на вход принимаем число (N), а на выходе показывает  все четные числа от 1 до N
+Пример:
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int even = 2;
+
+while (num >= even)
+{
+    Console.Write(even + " ");
+    even = even + 2;
+}
